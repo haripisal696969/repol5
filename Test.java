0a5bdf53e83b30338f94c2b6d60a7eaad9a79862
Ybhushan <bhushanpujari22.com>
2024-06-26 10:52:38 +0530

    Second one

---
+++ Test.java
@@ -0,0 +1,20 @@
+<html>
+<head>
+</head>
+<body>
+<cemter>
+<h1>Program To Add two number</h1>
+<script>
+<function sum(){
+x=parseInt(promt("Enter fist number:"));
+y=parseInt(promt('Enter second number:"));
+document.write(<h1>sum="+(x+y)+"</h1>);
+}
+sum();
+sum();
+sum();
+sum();
+</script>
+</center>
+</body>
+</html>
